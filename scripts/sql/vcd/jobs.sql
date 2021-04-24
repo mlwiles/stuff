2441e0b3f16283c7f127c735653e2eec73658b42
Michael Wiles <mwiles@us.ibm.com>
2021-04-24 01:01:02 -0400

    new powershell files
    renamed a few files
    added some new ones from work
    search for VMDK files for Proxies and relocate DLR for DCGroup

--- scripts/sql/vcd/jobs.sql
+++ scripts/sql/vcd/jobs.sql
@@ -6,3 +6,8 @@ SELECT * FROM public.jobs where status = 1
 --delete from public.task where job_id='a8624efe-0f2b-45d2-af38-3d24b2ccfd08'
 --select * from busy_object where task_id = '4d0ecc02-00c4-4aca-b92e-98a6321c545f'
 --delete from busy_object where task_id = '4d0ecc02-00c4-4aca-b92e-98a6321c545f'
+
+
+SELECT * FROM public.jobs where status = 1 and operation = 'VDC_DELETE_MEDIA'
+SELECT * FROM public.task where operation = 'VDC_DELETE_MEDIA'
+SELECT * FROM busy_object where task_id in (SELECT id FROM public.task where operation = 'VDC_DELETE_MEDIA')
