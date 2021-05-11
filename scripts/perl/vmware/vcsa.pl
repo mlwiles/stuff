fe589adef49c4f4e409c671bf570bb648503456a
Michael Wiles <mwiles@us.ibm.com>
2021-05-10 22:43:50 -0400

    IBM Freaked out ... for a useless token and my email

--- scripts/perl/vmware/vcsa.pl
+++ scripts/perl/vmware/vcsa.pl
@@ -13,7 +13,7 @@ use MIME::Base64;
 
 $ENV{'PERL_LWP_SSL_VERIFY_HOSTNAME'} = 0;
 
-my $username = 'mwiles@us.ibm.com';
+my $username = 'REDACTED';
 my $password = 'REDACTED';
 my $base64 = encode_base64($username . ':' . $password);
 print "Base64:" . $base64 . "\n";
@@ -21,7 +21,7 @@ print "Base64:" . $base64 . "\n";
 my $client = REST::Client->new();
 $client->setHost('https://hostname');
 
-my $headers = {Token => '5ade6d999f397790fa0ddd163b7d182e'};
+my $headers = {Token => 'REDACTED'};
 $client->GET('/rest/vcenter/datacenter', $headers);
 print $client->responseCode() . "\n";
 print $client->responseContent() . "\n";
