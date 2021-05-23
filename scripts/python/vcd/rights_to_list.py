726a6666a2f20fbf8d1336fd35bdaeb331a706d6
Michael Wiles <mwiles@us.ibm.com>
2021-05-23 16:30:26 -0400

    Update rights_to_list.py

--- scripts/python/vcd/rights_to_list.py
+++ scripts/python/vcd/rights_to_list.py
@@ -9,7 +9,7 @@
 # Expected output:
 #	csv ouput of rights
 # 
-import certifi, urllib3, json
+import certifi, urllib3, json, getpass
 from base64 import b64encode
 from requests.utils import requote_uri
 
@@ -18,17 +18,23 @@ host_list = [
     'https://fradir01.vmware-solutions.cloud.ibm.com',
     'https://sdaldir01.vmware-solutions.cloud.ibm.com',
     'https://sdaldir02.vmware-solutions.cloud.ibm.com',
-    'https://swdcdir01.vmware-solutions.cloud.ibm.com'
+    'https://sdaldir03.vmware-solutions.cloud.ibm.com',
+    'https://sdaldir04.vmware-solutions.cloud.ibm.com',
+    'https://swdcdir01.vmware-solutions.cloud.ibm.com',
+    'Enter your own URL...'
 ]
 for index in range(len(host_list)):
     print(f'{index+1}: {host_list[index]}')
 number = int(input("Which host do you want to connect to: "))
 rootURL = host_list[number-1]
 
+if number == len(host_list):
+    rootURL = str(input("Enter host full URL (https://host.vmware-solutions.cloud.ibm.com): "))
+
 print()
-sysusr = str(input("Enter your user id: "))
+sysusr = str(input("Enter your user id for " + rootURL + ": "))
 print()
-syspwd = str(input("Enter your password: "))
+syspwd = str(getpass.getpass("Enter your password for " + sysusr + ": "))
 print()
 sysorg = 'System'
 syslogin = "{0}@{1}".format(sysusr,sysorg)
@@ -59,15 +65,15 @@ def get_rights(rootURL, bearerToken, client, context):
     headers = { 'Authorization' : bearerToken, 'Accept' : 'application/json;version=34.0' }
     response = client.request("GET", url, headers=headers)
     # parse the response
-    data = json.loads(response._body.decode('UTF-8'))
+    data1 = json.loads(response._body.decode('UTF-8'))
 
     # find the default bundle
-    for x in range(len(data['values'])):
-        print(f"{x+1}: {data['values'][x]['name']}")
+    for x in range(len(data1['values'])):
+        print(f"{x+1}: {data1['values'][x]['name']}")
     number = int(input("Which item do you want to view: ")) 
     print()
 
-    encid = requote_uri(data['values'][number-1]['id'])
+    encid = requote_uri(data1['values'][number-1]['id'])
     
     pagecount = 1
     page = 1
@@ -77,12 +83,12 @@ def get_rights(rootURL, bearerToken, client, context):
         url = '{0}/cloudapi/1.0.0/{1}/{2}/rights?page={3}&pageSize=125'.format(rootURL,context,encid,page)
         headers = { 'Authorization' : bearerToken, 'Accept' : 'application/json;version=34.0' }
         response = client.request("GET", url, headers=headers)
-        data = json.loads(response._body.decode('UTF-8'))
-        pagecount = data['pageCount']
-        page = data['page']
+        data2 = json.loads(response._body.decode('UTF-8'))
+        pagecount = data2['pageCount']
+        page = data2['page']
         
         # get the rights
-        for i in data['values']:
+        for i in data2['values']:
             name = i['name']
             name = name.replace(",", "")
             name = name.replace(": ", ",")
@@ -97,6 +103,15 @@ def get_rights(rootURL, bearerToken, client, context):
     rights_list.sort()
     print(*rights_list, sep = "\n")
     print(len(rights_list))
+
+    filename = rootURL.replace("https://","")
+    filename += "."
+    filename += data1['values'][number-1]['name']
+    filename = filename.replace(" ",".")
+    with open(filename, 'w') as fileout:
+        for right in rights_list:
+            fileout.write(right)
+            fileout.write("\n")
 
 def main():
     client, bearerToken = login(rootURL)
